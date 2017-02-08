cd99c46287665b314ef56445db969e590f9e3ff8
phithakul <pthakul@hotmail.com>
2017-02-08 17:02:19 +0700

    add verify.php

---
+++ verify.php
@@ -0,0 +1,18 @@
+<?php
+$access_token = 'YBSRs8RsUu6uTjsOSA3aTB1brtR1zdOKTv4MYWpW/T9VXvYdwnRPNnwPv6VuUWXlNoRUsSYIRiXJz/y55+W6Dij1bEGVrqcLVoLTgMYeNCR0WiO9i0nWMuxAeTb2qQwGtzaNtf+rkgAuu/PttjuR7wdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v2/oauth/verify';
+
+$headers = array('Content-Type: application/x-www-form-urlencoded');
+$post_data = 'access_token=' . $access_token;
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
