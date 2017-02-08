4d5c8ef00afc217670f8e21599e0dcd341234c09
phithakul <pthakul@hotmail.com>
2017-02-08 17:19:14 +0700

    update verify.php

--- verify.php
+++ verify.php
@@ -4,13 +4,13 @@ $access_token = 'YBSRs8RsUu6uTjsOSA3aTB1brtR1zdOKTv4MYWpW/T9VXvYdwnRPNnwPv6VuUWX
 $url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Content-Type: application/x-www-form-urlencoded');
-$data = array('access_token=' => $access_token);
+$data = array('access_token' => $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data)); // http_build_query: Generate URL-encoded query string
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
